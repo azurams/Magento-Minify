793eedb7dcd54f36ac69182c77db7b089eca6d39
paales <paul@h-o.nl>
2012-11-09 21:59:43 +0100

    simplified init.js, only need to get watch mode working.

--- js/less/init.js
+++ js/less/init.js
@@ -1,23 +1,21 @@
 (function() {
+//    less.watch();
 
     var cssEls = document.getElementsByTagName('link');
     var init = false;
+
     for (var i in cssEls)
     {
         if (cssEls[i].href && cssEls[i].href.substr(-5) == '.less')
         {
             init = true;
-            cssEls[i].rel = 'stylesheet/less';
+            less.sheets.push(cssEls[i]);
         }
     }
 
     if (init)
     {
-        var scriptEls = document.getElementsByTagName('script');
-        var thisScriptEl = scriptEls[scriptEls.length - 1];
-        var scriptPath = thisScriptEl.src;
-        var scriptFolder = scriptPath.substr(0, scriptPath.lastIndexOf( '/' )+1 );
-
-        window.less || document.write('<script src="'+scriptFolder+'less.js"><\/script>');
+//        less.env = "development";
+        less.refresh();
     }
 })();
